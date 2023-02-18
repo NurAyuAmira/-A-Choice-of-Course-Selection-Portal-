7e3ef481e5abfe9e3326c48590f9409410c94ef0
yuz7_ <yuz9690@gmail.com>
2023-02-18 20:41:57 +0800

    Create UserSystem.java

---
+++ src/main/model/UserSystem.java
@@ -0,0 +1,18 @@
+//Authors:Ayu and Naufal
+package model;
+//UserSystem is an abstract class. Have the information fields declared in it.
+public abstract class UserSystem
+{
+
+    // Stores the level of study of the user
+    protected String level;
+
+    // Stores the system login id of the user
+    protected String id;
+
+    // Stores the name of the user
+    protected String name;
+
+    //Stores the system login password of the user
+    protected String password;
+}
