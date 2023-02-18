90a4b5febffbdab3e8f9a6c739d730ef6038f3ba
yuz7_ <yuz9690@gmail.com>
2023-02-18 20:50:03 +0800

    Create Main.java

---
+++ src/main/view/Main.java
@@ -0,0 +1,11 @@
+package view;
+
+//Authors: Naufal
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Initializing the constructor of WelcomeScreen - The entry point of the program.
+        new FirstScreenGUI();
+    }
+}
